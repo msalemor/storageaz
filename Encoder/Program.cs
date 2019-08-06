87828cd5241089abb051d7618074e559428b5ff8
Alex Morales <alemor@microsoft.com>
2019-08-06 11:59:40 -0400

    - Added comments

--- Encoder/Program.cs
+++ Encoder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Encoder.Models;
 using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 
@@ -10,7 +11,10 @@ namespace Encoder
     {
         static void Main(string[] args)
         {
+            // Get an ID from the tickcount
             var id = Environment.TickCount;
+
+            // Create a new contact
             var contact = new Contact
             {
                 ID = id,
@@ -20,26 +24,43 @@ namespace Encoder
                 DofB = new DateTime(1980, 1, 1)
             };
 
+            // Serialize the contact to JSON and Save it to a file
             SerializeAndSaveToFile(id, contact);
+
+            // Read the file and deserialize
             DeserializeAndTest(id);
         }
 
         private static void SerializeAndSaveToFile(int id, Contact contact)
         {
+            // Serialize to JSON
             var json = JsonConvert.SerializeObject(contact);
+            // Serialize to Base64
             var b64bytes = Convert.ToBase64String(Encoding.ASCII.GetBytes(json));
+            // Write Base64 to file
             var file = $"contact{id}.b64";
             File.WriteAllBytes(file, Encoding.ASCII.GetBytes(b64bytes));
+            Console.WriteLine($"File: {file} created");
         }
 
         private static void DeserializeAndTest(int id)
         {
+            // Read the Base64 file
             var file = $"contact{id}.b64";
             var buffer = File.ReadAllBytes(file);
+            // Deserialize Base64
             var base64string = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
             byte[] base64bytes = Convert.FromBase64String(base64string);
+            // Deserialize to JSON
             var json = Encoding.UTF8.GetString(base64bytes, 0, base64bytes.Length);
+            // Deserialize JSON to .Net object
             var contact = JsonConvert.DeserializeObject<Contact>(json);
+            // Test that the IDs match
+            if (id == contact.ID)
+            {
+                Console.WriteLine("Test passed.");
+            }
+
         }
     }
 }
--- ProcessBlob/Function1.cs
+++ ProcessBlob/Function1.cs
@@ -12,8 +12,9 @@ namespace ProcessBlob
 {
     public static class Function1
     {
-        //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+        // CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
         static CloudStorageAccount storageAccount = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+
         // Create the destination blob client
         static CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
 
@@ -21,9 +22,15 @@ namespace ProcessBlob
         public static async Task Run([BlobTrigger("process-container/{name}", Connection = "")]CloudBlockBlob myBlob, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{myBlob.Properties.Length} \n Size: ? Bytes");
+
+            // Once the blob is posted to the process-container process it
             var json = await ProcessAsync(myBlob, log);
             log.LogInformation("Saving the json payload after processing");
+
+            // Save the JSON that was deserialized to a blob on the json-container
             await SaveBlob(json, log);
+
+            // Move the blob from the original location to the archive-container
             log.LogInformation("Moving the processed blob to archive");
             await MoveBlob(myBlob, log);
 
@@ -31,18 +38,24 @@ namespace ProcessBlob
 
         private async static Task<string> ProcessAsync(CloudBlockBlob blob, ILogger log)
         {
+            // Read the blob's bytes
             var buffer = new byte[blob.Properties.Length];
             await blob.DownloadToByteArrayAsync(buffer, 0);
 
+            // Convert the bytes into a Base64 string
             var base64Encoded = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
             byte[] data = Convert.FromBase64String(base64Encoded);
+
+            // Convert the Base64 to the JSON string
             return Encoding.UTF8.GetString(data, 0, data.Length);
         }
 
         private async static Task SaveBlob(string json, ILogger log)
         {
 
+            // Get a reference to the json-container
             CloudBlobContainer container = blobClient.GetContainerReference("json-container");
+
             // Create the container if it doesn't already exist.
             try
             {
@@ -52,17 +65,25 @@ namespace ProcessBlob
             {
                 log.LogError(e.Message);
             }
+            
+            // Get a blob reference for the new blob
             var blobName = $"{Environment.TickCount.ToString()}.json";
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobName);
+
+            // Get the bytes for the JSON
             var buffer = Encoding.ASCII.GetBytes(json);
             var count = json.Length;
-            log.LogInformation($"Blob {blobName} saved to container json-container");
+            
+            // Save the bytes to blob reference
             await blockBlob.UploadFromByteArrayAsync(buffer, 0, count);
+            log.LogInformation($"Blob {blobName} saved to container json-container");
         }
 
         private async static Task MoveBlob(CloudBlockBlob myBlob, ILogger log)
         {
+            // Get a refence to the archive-container
             CloudBlobContainer container = blobClient.GetContainerReference("archive-container");
+
             // Create the container if it doesn't already exist.
             try
             {
@@ -80,7 +101,7 @@ namespace ProcessBlob
             log.LogInformation("Starting Copy");
             try
             {
-                // Use this method to copy to blob 
+                // Copy the original blob to the new location
                 await blockBlob.StartCopyAsync(myBlob);
 
                 // alternatively, uncomment the code below and use this method to copy the blob data
@@ -89,6 +110,8 @@ namespace ProcessBlob
                   await blockBlob.UploadFromStreamAsync(stream);
                 }*/
                 log.LogInformation("Starting Delete");
+
+                // Delete the original blob
                 await myBlob.DeleteAsync();
                 log.LogInformation("Move completed");
 
